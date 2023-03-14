7d47374278601961cb5dae8ce680db257a53bbbf
Choi GD <ChoiGD0307@gmail.com>
2023-03-14 13:51:46 +0900

    react init

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import Users from './user/pages/Users';
+import NewPlaces from './places/pages/NewPlaces';
+
+
+const App = () => {
+  return <Router>
+    <Switch>
+      <Route path="/" exact>
+        <Users />
+      </Route>
+      <Route path="/places/new" exact>
+        <NewPlaces />
+      </Route>
+      <Redirect to="/" />
+    </Switch>
+  </Router>;
+}
+
+export default App;
---
+++ src/places/pages/NewPlaces.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewPlaces = ()=>{
+    return <h2>NewPlaces Pages!!!!!</h2>;
+};
+
+export default NewPlaces;
