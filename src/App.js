5395a3e953b525e29e4493e85caa1a3d2ce991cd
Choi GD <ChoiGD0307@gmail.com>
2023-03-21 14:29:54 +0900

    UserPlaces + css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import Users from './user/pages/Users';
 import NewPlaces from './places/pages/NewPlaces';
+import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
@@ -11,6 +12,9 @@ const App = () => {
       <Switch>
         <Route path="/" exact>
           <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
         </Route>
         <Route path="/places/new" exact>
           <NewPlaces />
