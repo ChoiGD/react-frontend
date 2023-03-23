03a9a21627f7c4af261de2675aef39436806cd33
Choi GD <ChoiGD0307@gmail.com>
2023-03-23 16:45:31 +0900

    validators

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -3,10 +3,11 @@ import React from "react";
 import './NewPlace.css';
 
 import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
 
 const NewPlace = ()=>{
     return <form className="place-form">
-        <Input element="input" type="text" label="Title" errorText="Please enter a valid title."/>
+        <Input element="input" type="text" label="Title" errorText="Please enter a valid title." validators={[VALIDATOR_REQUIRE()]}/>
     </form>
 };
 
